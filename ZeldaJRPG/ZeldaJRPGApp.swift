593b663746db3d7c86ecf4cf4c1166b384df62cc
Kyle Peterson <kdpeters@gmail.com>
2023-08-09 09:43:25 -0400

    Initial Commit

---
+++ ZeldaJRPG/ZeldaJRPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeldaJRPGApp.swift
+//  ZeldaJRPG
+//
+//  Created by Kyle Peterson on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ZeldaJRPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
