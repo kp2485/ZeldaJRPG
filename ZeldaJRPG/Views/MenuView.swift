b8b467cce7ab73c08ee37b5345b8144d58710141
Kyle Peterson <kdpeters@gmail.com>
2023-09-05 09:10:53 -0400

    refactored menu view, temporarily disabled animations

--- ZeldaJRPG/Views/MenuView.swift
+++ ZeldaJRPG/Views/MenuView.swift
@@ -13,127 +13,133 @@ struct MenuView: View {
     
     var body: some View {
         ZStack {
-            Image("goldBackground")
-                .resizable()
-                .scaledToFill()
-                .edgesIgnoringSafeArea(.all)
+            background
             
             VStack {
                 Spacer()
                 
-                Image("Zelda_old_logo")
-                    .resizable()
-                    .scaledToFit()
-                    .padding(.horizontal, 30)
-                    
+                zeldaLogo
                 
-                Text("Quantum Link")
-                    .font(.custom("HyliaSerifBeta-Regular", size: 40))
-                    .baselineOffset(5)
-                    .opacity(0.60)
-                    .kerning(5)
+                gameTitle
                 
                 ZStack{
                     // TODO: Triforce underlay
                     
                     VStack {
                         topTriforceButton
-                            .modifier(RotateOnTap(isRotating: isRotating, degrees: 720.0))
-                        
                         
                         HStack {
                             leftTriforceButton
-                                .modifier(RotateOnTap(isRotating: isRotating, degrees: 720.0))
+                            
                             rightTriforceButton
-                                .modifier(RotateOnTap(isRotating: isRotating, degrees: 720.0))
                         }
                     }
                 }
                 
-                //                Button {
-                //                    // TODO: Start Group Selection Menu
-                //                } label: {
-                //                    topTriforceButton
-                //                }
-                //                .padding(.vertical, 15)
-                //                .padding(.horizontal, 30)
-                //                .background(
-                //                    RoundedRectangle(cornerRadius: 30)
-                //                        .fill(matchManager.authenticationState != .authenticated || matchManager.inGame ? .brown : .gray)
-                //                        .opacity(0.5)
-                //                )
-                //
-                //                Button {
-                //                    // TODO: Start Matchmaking Menu
-                //                } label: {
-                //                    Text("MATCHMAKING")
-                //                        .font(.largeTitle)
-                //                        .bold()
-                //                }
-                //                .disabled(matchManager.authenticationState != .authenticated || matchManager.inGame)
-                //                .padding(.vertical, 15)
-                //                .padding(.horizontal, 30)
-                //                .background(
-                //                    RoundedRectangle(cornerRadius: 30)
-                //                        .fill(matchManager.authenticationState != .authenticated || matchManager.inGame ? .brown : .gray)
-                //                        .opacity(0.5)
-                //                )
+                
+                Text(matchManager.authenticationState.rawValue)
+                    .font(.headline.weight(.semibold))
+                    .opacity(0.6)
+                
                 
                 Spacer()
+                
             }
         }
         
     }
     
+    var background: some View {
+        Image("goldBackground")
+            .resizable()
+            .scaledToFill()
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    var zeldaLogo: some View {
+        Image("Zelda_old_logo")
+            .resizable()
+            .scaledToFit()
+            .padding(.horizontal, 30)
+    }
+    
+    var gameTitle: some View {
+        // TODO: Animate letter by letter from ancient hylian to english
+        Text("Quantum Link")
+            .font(.custom("HyliaSerifBeta-Regular", size: 40))
+            .baselineOffset(5)
+            .opacity(0.6)
+            .kerning(5)
+    }
+    
     var topTriforceButton: some View {
-        ZStack {
-            triforceButtonBackground
-            Image("TAoL_Magical_Sword_Artwork")
-                .resizable()
-                .scaledToFit()
-                .rotationEffect(Angle(degrees: 210))
-                .frame(height: 100)
-                .offset(x: -15, y: 6)
-            Image("TAoL_Magical_Sword_Artwork")
-                .resizable()
-                .scaledToFit()
-                .rotationEffect(Angle(degrees: 150))
-                .frame(height: 100)
-                .offset(x: 15, y: 6)
+        Button {
+            print("Battle button tapped")
+            // TODO: Start Matchmaking Menu
+        } label: {
+            ZStack {
+                triforceButtonBackground
+                    .foregroundColor(matchManager.authenticationState != .authenticated || matchManager.inGame ? .black : .white)
+                Image("TAoL_Magical_Sword_Artwork")
+                    .resizable()
+                    .scaledToFit()
+                    .rotationEffect(Angle(degrees: 210))
+                    .frame(height: 100)
+                    .offset(x: -15, y: 6)
+                Image("TAoL_Magical_Sword_Artwork")
+                    .resizable()
+                    .scaledToFit()
+                    .rotationEffect(Angle(degrees: 150))
+                    .frame(height: 100)
+                    .offset(x: 15, y: 6)
+            }
+            //            .modifier(RotateOnTap(isRotating: isRotating, degrees: 720.0))
         }
+        //        .disabled(matchManager.authenticationState != .authenticated || matchManager.inGame)
     }
     
     var leftTriforceButton: some View {
-        ZStack {
-            triforceButtonBackground
-            Image("LinkSwordRaised")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 150)
-                .offset(x: -18, y: 7)
-            Image("Zeldafs2")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 60)
-                .offset(x: 22, y: 22)
+        Button {
+            print("Character button tapped")
+        } label: {
+            ZStack {
+                triforceButtonBackground
+                    .foregroundColor(.white)
+                Image("LinkSwordRaised")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                    .offset(x: -18, y: 7)
+                Image("Zeldafs2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 60)
+                    .offset(x: 22, y: 22)
+            }
+            //            .modifier(RotateOnTap(isRotating: isRotating, degrees: 720.0))
         }
     }
     
     var rightTriforceButton: some View {
-        ZStack {
-            triforceButtonBackground
-            Image("ALttP_Stalfos_Knight_Sprite")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 90)
-                .offset(y: 11)
+        Button {
+            print("Enemies button tapped")
+        } label: {
+            ZStack {
+                triforceButtonBackground
+                    .foregroundColor(.white)
+                Image("ALttP_Stalfos_Knight_Sprite")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 90)
+                    .offset(y: 11)
+            }
         }
+        //        .modifier(RotateOnTap(isRotating: isRotating, degrees: 720.0))
     }
     
     var triforceButtonBackground: some View {
         Image(systemName: "triangle.fill", variableValue: 1.0)
             .font(.system(size: 130))
-            .foregroundColor(.white)
             .opacity(0.5)
             .shadow(radius: CGFloat(5))
     }
